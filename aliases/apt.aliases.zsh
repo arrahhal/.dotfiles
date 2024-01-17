f8abe9f570651bb16b6c3a919a25d722935bc26e
arrahhal <arrahhal@proton.me>
2024-01-17 12:41:52 +0300

    shortify apt aliases a bit

--- aliases/apt.aliases.zsh
+++ aliases/apt.aliases.zsh
@@ -1,9 +1,9 @@
 alias apts='apt-cache search'
 alias aptsh='apt-cache show'
 alias apti='sudo apt-get install -V'
-alias aptupd='sudo apt-get update'
-alias aptupg='sudo apt-get dist-upgrade -V && sudo apt-get autoremove'
-alias aptupgd='sudo apt-get update && sudo apt-get dist-upgrade -V && sudo apt-get autoremove'
+alias aptu='sudo apt-get update'
+alias aptg='sudo apt-get dist-upgrade -V && sudo apt-get autoremove'
+alias aptug='sudo apt-get update && sudo apt-get dist-upgrade -V && sudo apt-get autoremove'
 alias aptr='sudo apt-get remove'
 alias aptpurge='sudo apt-get remove --purge'
 
